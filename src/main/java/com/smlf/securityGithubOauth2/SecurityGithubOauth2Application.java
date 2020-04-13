c8940a3cd9f339e80e42e78cb28b61876407854a
levi.wang <levi.wang@ihr360.com>
2020-04-13 22:06:04 +0800

    first commit

---
+++ src/main/java/com/smlf/securityGithubOauth2/SecurityGithubOauth2Application.java
@@ -0,0 +1,13 @@
+package com.smlf.securityGithubOauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityGithubOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityGithubOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/smlf/securityGithubOauth2/SecurityGithubOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smlf.securityGithubOauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityGithubOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
